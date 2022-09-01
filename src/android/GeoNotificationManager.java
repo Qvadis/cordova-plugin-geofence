9bafb76cc806616cfd2b62a418d0beeccd364182
Borja Antona <bantona@qvadis.es>
2022-09-01 17:51:36 +0200

    - Removed flag INITIAL_TRIGGER_ENTER

--- src/android/GeoNotificationManager.java
+++ src/android/GeoNotificationManager.java
@@ -55,7 +55,7 @@ public class GeoNotificationManager implements OnCompleteListener<Void> {
         // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
         // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
         // is already inside that geofence.
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        // builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
 
         // Add the geofences to be monitored by geofencing service.
         builder.addGeofences(mGeofenceList);
@@ -86,7 +86,7 @@ public class GeoNotificationManager implements OnCompleteListener<Void> {
             // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
             // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
             // is already inside that geofence.
-            builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+            // builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
 
             // Add the geofences to be monitored by geofencing service.
             builder.addGeofences(geoFences);
